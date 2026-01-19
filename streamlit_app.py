2b13150a79e0350404358640af01c79d8c9cffbd
kinaniwm111-afk <kinaniwm111@gmail.com>
2026-01-20 01:42:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("GovSense Voice AI")
+
+st.write("Demo: Voice-based complaint analysis")
+
+if st.button("Analyze Complaint"):
+    st.write("Emotion: High Anger")
+    st.write("Category: Service Delay")
+    st.write("Priority: URGENT")
